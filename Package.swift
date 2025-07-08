34f3d9d06695fcb693afbebe3f39395d8258ad15
Uhucream <aborog1324@yahoo.co.jp>
2025-07-09 03:46:00 +0900

    chore: add dependency of swift-package-manager-google-mobile-ads

--- Package.swift
+++ Package.swift
@@ -9,7 +9,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AdMobUI",
-            targets: ["AdMobUI"]),
+            targets: ["AdMobUI"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            name: "GoogleMobileAds",
+            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+            .upToNextMajor(
+                from: "12.6.0"
+            )
+        )
     ],
     dependencies: [
         .package(
@@ -21,7 +31,13 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AdMobUI"),
+            name: "AdMobUI",
+            dependencies: [
+                .byName(
+                    name: "GoogleMobileAds"
+                )
+            ]
+        ),
 
     ]
 )
